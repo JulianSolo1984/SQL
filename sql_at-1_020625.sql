fd4e1064ed6f996ea1ab587b0a60732657e9f47a
JulianSolo <juliansolowork@gmail.com>
2025-06-02 21:21:53 -0300

    sql_at-1_020625.sql

---
+++ sql_at-1_020625.sql
@@ -0,0 +1,69 @@
+/* SELECT names */
+
+/* Q1 */
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+/* Q2 */
+SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+/* Q3 */
+SELECT name FROM world
+  WHERE name LIKE '%X%'
+
+/* Q4 */
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+/* Q5 */
+SELECT name FROM world
+  WHERE name LIKE 'C%' AND name LIKE '%ia'
+
+/* Q6 */
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+/* Q7 */
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+/* Q8 */
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+/* Q9 */
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+
+/* Q10 */
+SELECT name FROM world
+ WHERE LENGTH(name) = 4;
+
+/* Q11 */
+SELECT name
+  FROM world
+ WHERE name = capital
+
+/* Q12 */
+SELECT name
+  FROM world
+ WHERE capital = CONCAT(name, ' City')
+
+/* Q13 */
+SELECT capital, name
+FROM world
+WHERE capital LIKE CONCAT('%', name, '%')
+
+/* Q14 */
+SELECT capital, name
+FROM world
+WHERE capital LIKE CONCAT(name, ' %')
+OR capital LIKE CONCAT(name, '-%');
+
+/* Q15 */
+SELECT name, REPLACE(capital, name, '')
+   FROM world
+WHERE capital LIKE CONCAT(name,'%') 
+   AND capital <> name
